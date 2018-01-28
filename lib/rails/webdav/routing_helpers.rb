494a8ca76a40d06607ca942355e8d19f3bcf309d
Kenaniah Cerny <kenaniah@gmail.com>
2018-01-28 13:06:59 -0800

    checkpoint

--- lib/rails/webdav/routing_helpers.rb
+++ lib/rails/webdav/routing_helpers.rb
@@ -1,46 +1,41 @@
 module Rails
 	module WebDAV
 		module RoutingHelpers
-			def webdav_collection *resources, &block
+			def webdav_resource *resources, &block
+
 				options = resources.extract_options!.dup
-				if apply_common_behavior_for(:webdav_collection, resources, options, &block)
+				if apply_common_behavior_for(:webdav_resource, resources, options, &block)
 					return self
 				end
-				with_scope_level :resources do
+
+				with_scope_level @scope[:options][:routing_mode] do
+
 					options = apply_action_options options
+					options.delete :routing_mode
 					resource_scope(ActionDispatch::Routing::Mapper::Resources::Resource.new(resources.pop, api_only?, @scope[:shallow], options)) do
+
 						yield if block_given?
+
 						concerns(options[:concerns]) if options[:concerns]
-						collection do
-							[:propfind, :options, :head, :get].each do |m|
-								match "", action: :webdav, via: m
-							end
-						end
-						member do
-							[:propfind, :options, :head, :get].each do |m|
-								match "", action: :webdav, via: m
-							end
-						end
-					end
-				end
-			end
-			def webdav_file *resources, &block
-				options = resources.extract_options!.dup
-				if apply_common_behavior_for(:webdav_file, resources, options, &block)
-					return self
-				end
-				with_scope_level :resource do
-					options = apply_action_options options
-					resource_scope(ActionDispatch::Routing::Mapper::Resources::Resource.new(resources.pop, api_only?, @scope[:shallow], options)) do
-						yield if block_given?
-						concerns(options[:concerns]) if options[:concerns]
+
 						collection do
 							[:propfind, :options].each do |m|
 								match "", action: :webdav, via: m
 							end
 						end
+
+						if @scope[:options][:routing_mode] == :resources
+							member do
+								[:propfind, :options, :head, :get].each do |m|
+									match "", action: :webdav, via: m
+								end
+							end
+						end
+
 					end
+
 				end
+
 			end
 		end
 	end
